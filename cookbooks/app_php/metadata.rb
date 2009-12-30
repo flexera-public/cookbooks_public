6540db809fb499e81d3d0e328a574cff1a047745
Jeremy Deininger <jeremy@rightscale.com>
2009-12-29 17:22:11 -0800

    metadata fix for Refs #3348 - adding :recipes => for UI conflict with AIO

--- cookbooks/app_php/metadata.rb
+++ cookbooks/app_php/metadata.rb
@@ -24,22 +24,26 @@ attribute "php",
 attribute "php/db_app_user",
   :display_name => "Database User",
   :description => "If the MySQL administrator set up a restricted MySQL account for application servers to access the database, then specify the username of that account for this input.  If there is not a restricted MySQL account then use the same value that's used for 'Database Admin Username'.  The application server will then have unrestricted access to the database.",
-  :required => true
+  :required => true,
+  :recipes => [ "app_php::do_update_code", "app_php::do_db_restore", "app_php::install_php" ]
 
 attribute "php/db_app_passwd",
   :display_name => "Database Password",
   :description => "If the MySQL administrator set up a restricted MySQL account for application servers to access the database, then specify the password of that account for this input.  If there is not a restricted MySQL account then use the same value that's used for 'Database Admin Password'.  The application server will then have unrestricted access to the database.",
-  :required => true
+  :required => true,
+  :recipes => [ "app_php::do_update_code", "app_php::do_db_restore", "app_php::install_php" ]
 
 attribute "php/db_schema_name",
   :display_name => "Database Schema Name",
   :description => "Enter the name of the MySQL database schema to which applications will connect.  The database schema was created when the initial database was first set up.  This input will be used to set the application server's database config file so that applications can connect to the correct schema within the database.  This input is also used for MySQL dump backups in order to determine which schema is getting backed up.  Ex: mydbschema",
-  :required => true
+  :required => true,
+  :recipes => [ "app_php::do_update_code", "app_php::do_db_restore", "app_php::install_php" ]
 
 attribute "php/db_dns_name",
   :display_name => "Database Dns Name",
   :description => "The fully qualified domain name of the database server to which the application server(s) will connect.  Ex: master.mydatabase.com",
-  :required => true
+  :required => true,
+  :recipes => [ "app_php::do_update_code", "app_php::do_db_restore", "app_php::install_php" ]
 
 attribute "php/code",
   :display_name => "PHP Application Code",
@@ -48,13 +52,15 @@ attribute "php/code",
 attribute "php/code/url",
   :display_name => "Repository URL",
   :description => "Specify the URL location of the repository that contains the application code. Ex: git://github.com/mysite/myapp.git",
-  :required => true
+  :required => true,
+  :recipes => [ "app_php::do_update_code", "app_php::do_db_restore", "app_php::install_php" ]
 
 attribute "php/code/credentials",
   :display_name => "Repository Credentials",
   :description => "The private SSH key of the git repository.",
   :required => false,
-  :default => ""  
+  :default => "",
+  :recipes => [ "app_php::do_update_code", "app_php::do_db_restore", "app_php::install_php" ]  
 
 #
 # recommended attributes
@@ -62,16 +68,19 @@ attribute "php/code/credentials",
 attribute "php/server_name",
   :display_name => "Server Name",
   :description => "The fully qualified domain name of the application server used to define your virtual host.",
-  :required => true
+  :required => true,
+  :recipes => [ "app_php::do_update_code", "app_php::do_db_restore", "app_php::install_php" ]
 
 attribute "php/application_name",
   :display_name => "Application Name",
   :description => "Sets the directory for your application's web files (/home/webapps/Application Name/current/).  If you have multiple applications, you can run the code checkout script multiple times, each with a different value for APPLICATION, so each application will be stored in a unique directory.  This must be a valid directory name.  Do not use symbols in the name.",
-  :default => "myapp"
+  :default => "myapp",
+  :recipes => [ "app_php::do_update_code", "app_php::do_db_restore", "app_php::install_php" ]
   
 attribute "php/db_mysqldump_file_path",
   :display_name => "Mysqldump File Path",
-  :description => "This input allows you to restore your database by choosing a specific MySQL database backup file.  You will need to specify a full path and/or filename.  Ex: branch/mydb-200910300402.gz"
+  :description => "This input allows you to restore your database by choosing a specific MySQL database backup file.  You will need to specify a full path and/or filename.  Ex: branch/mydb-200910300402.gz",
+  :recipes => [ "app_php::do_db_restore" ]
 
 #
 # optional attributes
@@ -80,15 +89,18 @@ attribute "php/db_mysqldump_file_path",
 attribute "php/code/branch",
   :display_name => "Repository Branch",
   :description => "The name of the branch within the git repository where the application code should be pulled from.",
-  :default => "master"
+  :default => "master",
+  :recipes => [ "app_php::do_update_code", "app_php::do_db_restore", "app_php::install_php" ]
   
 attribute "php/application_port",
   :display_name => "Application Port",
   :description => "This input is normally set to 8000 if this server is a combined HAProxy and application server. If this is an application server (w/o HAproxy), set it to 80.  When setting this in a deployment, you should use 80 at the deployment level since you want all of your servers in the array to use this value.  If the server is a FE+APP server, you can set it to 8000 at the server level so that it overrides the deployment level input.",
-  :default => "8000"
+  :default => "8000",
+  :recipes => [ "app_php::do_update_code", "app_php::do_db_restore", "app_php::install_php" ]
 
 attribute "php/modules_list",
   :display_name => "PHP modules",
   :discription => "An optional list of php modules to install",
-  :type => "array" 
+  :type => "array",
+  :recipes => [ "app_php::do_update_code", "app_php::do_db_restore", "app_php::install_php" ] 
 
