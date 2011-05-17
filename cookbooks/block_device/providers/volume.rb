ba62d4ff38054864533281f885ef8f0a2cfa7eaf
caryp <cary@rightscale.com>
2011-05-17 16:19:47 -0700

    fixed syntax error

--- cookbooks/block_device/providers/volume.rb
+++ cookbooks/block_device/providers/volume.rb
@@ -2,8 +2,8 @@
 action :create do
   device = init("volume", new_resource)
   create_options = {
-    :volume_size = new_resource.volume_size
-    :stripe_count = new_resource.stripe_count
+    :volume_size => new_resource.volume_size
+    :stripe_count => new_resource.stripe_count
   }
   device.action_create(create_options)
 end
