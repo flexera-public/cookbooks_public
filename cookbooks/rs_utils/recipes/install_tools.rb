6ebcda5cc8a5de1b30b8c6c516d0e3ef8a1011a1
caryp <cary@rightscale.com>
2011-05-24 12:18:48 -0700

    [rs_utils] updated tools gem to 0.1.2

--- cookbooks/rs_utils/recipes/install_tools.rb
+++ cookbooks/rs_utils/recipes/install_tools.rb
@@ -25,7 +25,7 @@
 
 
 SANDBOX_BIN_DIR = "/opt/rightscale/sandbox/bin"
-RESOURCE_GEM = ::File.join(::File.dirname(__FILE__), "..", "files", "default", "rightscale_tools-0.1.0.gem")
+RESOURCE_GEM = ::File.join(::File.dirname(__FILE__), "..", "files", "default", "rightscale_tools-0.1.2.gem")
 RACKSPACE_GEM = ::File.join(::File.dirname(__FILE__), "..", "files", "default", "right_rackspace-0.0.0.gem")
 
 r = gem_package RACKSPACE_GEM do
@@ -37,7 +37,7 @@ r.run_action(:install)
 
 r = gem_package RESOURCE_GEM do
   gem_binary "#{SANDBOX_BIN_DIR}/gem"
-  version "0.1.0"
+  version "0.1.2"
   action :nothing
 end
 r.run_action(:install)
