a3b76068b40a87959be880a274951dd4da19303b
caryp <cary@rightscale.com>
2011-05-25 11:59:35 -0700

    [block_device] added do_attach recipe to metadata

--- cookbooks/block_device/metadata.rb
+++ cookbooks/block_device/metadata.rb
@@ -4,3 +4,14 @@ license          "All rights reserved"
 description      "Installs/Configures block_device"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
+
+recipe  "block_device::do_attach", "creates, format and mount block_device."
+
+attribute "block_device/storage_type",
+  :display_name => "Block Device Storage Type",
+  :description => "Sets storage type to Volume (i.e.EBS) or Remote Object Store (i.e. s3, cloudfiles)",
+  :choice => ["volume", "ros"],
+  :type => "string",
+  :required => true,
+  :recipes => [ "rightscale_tools::do_attach" ]
+
