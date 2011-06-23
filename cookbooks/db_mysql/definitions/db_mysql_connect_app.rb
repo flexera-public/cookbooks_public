9dd7438c275b43e4de15d2fcb21107db640d9809
Erik Kargard <erik@rightscale.com>
2011-06-23 00:12:23 +0000

    [db_mysql] Always install MySQL 5.0 client

--- cookbooks/db_mysql/definitions/db_mysql_connect_app.rb
+++ cookbooks/db_mysql/definitions/db_mysql_connect_app.rb
@@ -24,11 +24,11 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 define :db_mysql_connect_app, :template => "db_connection_example.erb", :cookbook => "db_mysql", :database => nil do
-  if params[:version] == "5.0" 
+#  if params[:version] == "5.0" 
     include_recipe "db_mysql::install_client_5.0"
-  else
-    include_recipe "db_mysql::install_client"
-  end
+#  else
+#    include_recipe "db_mysql::install_client"
+#  end
 
   template params[:name] do
     source params[:template]
