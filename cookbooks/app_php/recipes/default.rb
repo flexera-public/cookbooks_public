441dfca48a56506fc1f5cc211c813f31b8a1af22
kbockmanrs <kevin@rightscale.com>
2011-07-14 21:46:10 +0000

    [app_php] using IUS repository for php53 install

--- cookbooks/app_php/recipes/default.rb
+++ cookbooks/app_php/recipes/default.rb
@@ -22,6 +22,23 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+# Install the IUS repo
+remote_file "/root/ius-release-1.0-8.ius.el5.noarch.rpm" do
+  source "ius-release-1.0-8.ius.el5.noarch.rpm"
+end
+
+remote_file "/root/epel-release-1-1.ius.el5.noarch.rpm" do
+  source "epel-release-1-1.ius.el5.noarch.rpm"
+end
+
+bash "rpm install of IUS repository" do
+  code <<EOF
+  rpm -Uvh /root/epel-release-1-1.ius.el5.noarch.rpm
+  rpm -Uvh /root/ius-release-1.0-8.ius.el5.noarch.rpm
+  yum install -y yum-plugin-replace
+EOF
+end
+
 # == Install user-specified Packages and Modules
 #
 [ node[:php][:package_dependencies] | node[:php][:modules_list] ].flatten.each do |p|
