c6f7cfc5d40b3fd949243469b4d919fca2a09b0d
caryp <cary@rightscale.com>
2011-07-14 10:46:26 -0700

    [app] open port 8000 no 80 to LBs

--- cookbooks/app/providers/default.rb
+++ cookbooks/app/providers/default.rb
@@ -18,7 +18,7 @@ action :firewall_set do
   client_tag = new_resource.firewall_client_tag
   rs_utils_firewall_rules "Open AppServer ports to all taged servers" do
     machine_tag client_tag
-    port 80
+    port 8000
     enable to_enable
   end
 end
