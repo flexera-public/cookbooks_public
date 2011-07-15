49aabbfac1beee17f4c04522033b2c0638920839
caryp <cary@rightscale.com>
2011-07-14 23:34:29 -0700

    [app],[db] rake metadata

--- cookbooks/app/metadata.rb
+++ cookbooks/app/metadata.rb
@@ -7,7 +7,7 @@ version          "0.1"
 
 depends "sys_firewall"
 
-recipe "app:default", "Tags your server as an appserver. This is used by servers (like databases) to identify their clients."
+recipe "app::default", "Tags your server as an appserver. This is used by servers (like databases) to identify their clients."
 recipe "app::do_loadballancer_allow", "Allow connections from all loadbalancers in the deployment.  This should be run on an appserver before requesting connection to loadbalancers."
 recipe "app::do_loadballancers_deny", "Deny connections from all loadbalancers in the deployment. This can be run on an appserver to deny connections from all loadbalancers in the deployment."
 recipe "app::request_loadballancer_allow", "Sends request to all application servers to allow connections from the caller's private IP address. This should be run on a loadbalancer before attaching application servers."
