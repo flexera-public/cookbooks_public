f0f5d95666e6ea7b3c52019daadf65886e48ca0d
caryp <cary@rightscale.com>
2011-07-22 15:01:37 -0700

    [db_mysql] setup_monitoring failed to run twice

--- cookbooks/db_mysql/recipes/setup_monitoring.rb
+++ cookbooks/db_mysql/recipes/setup_monitoring.rb
@@ -3,36 +3,34 @@ service "collectd" do
 end
 
 if node[:platform] == 'centos'
-  if node[:kernel][:machine] == "x86_64"
-    ruby_block "install collectd-mysql rpms x86_64" do
-      block do
-        packages = ::File.join(::File.dirname(__FILE__), "..", "files", "centos", "*64.rpm")
-        packages.each do |package|
-          Chef::Log.info `rpm -i #{package} --nodeps`
-          raise "FATAL: error installing rpm #{package}" unless $?.success?
-        end
-      end
-    end
-  else
-    ruby_block "install collectd-mysql rpms i386" do
-      block do
-        packages = ::File.join(::File.dirname(__FILE__), "..", "files", "centos", "*i386.rpm")
-        packages.each do |package|
-          Chef::Log.info `rpm -i #{package} --nodeps`
-          raise "FATAL: error installing rpm #{package}" unless $?.success?
-        end
-      end
-    end
+
+  TMP_FILE = "/tmp/collectd.rpm"
+
+  remote_file TMP_FILE do
+    source "collectd-mysql-4.10.0-4.el5.#{node[:kernel][:machine]}.rpm"
+    only_if { node[:platform] == 'centos' }
+  end
+
+  package TMP_FILE do
+    source "/tmp/collectd.rpm"
+    only_if { node[:platform] == 'centos' }
   end
 
   template File.join(node[:rs_utils][:collectd_plugin_dir], 'mysql.conf') do
     backup false
     source "mysql_collectd_plugin.conf.erb"
     notifies :restart, resources(:service => "collectd")
+    only_if { node[:platform] == 'centos' }
   end
-
+  
 else
-  Chef::Log.info "WARNING: attempting to install collectd-mysql on unsupported platform #{node[:platform]}, continuing.."
+  
+  log "WARNING: attempting to install collectd-mysql on unsupported platform #{node[:platform]}, continuing.." do
+    level :warn
+  end
+  
 end
+
+
 
 
