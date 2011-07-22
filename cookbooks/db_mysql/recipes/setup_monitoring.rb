d644a5a29f80549adaa24605c2429bf3d1f4dfa5
caryp <cary@rightscale.com>
2011-07-22 15:51:21 -0700

    [db_mysq;] handle i686 arch for monitoring as well

--- cookbooks/db_mysql/recipes/setup_monitoring.rb
+++ cookbooks/db_mysql/recipes/setup_monitoring.rb
@@ -2,25 +2,25 @@ service "collectd" do
   action :nothing
 end
 
+arch = node[:kernel][:machine]
+arch = "i386" if arch == "i686"
+
 if node[:platform] == 'centos'
 
   TMP_FILE = "/tmp/collectd.rpm"
 
   remote_file TMP_FILE do
-    source "collectd-mysql-4.10.0-4.el5.#{node[:kernel][:machine]}.rpm"
-    only_if { node[:platform] == 'centos' }
+    source "collectd-mysql-4.10.0-4.el5.#{arch}.rpm"
   end
 
   package TMP_FILE do
-    source "/tmp/collectd.rpm"
-    only_if { node[:platform] == 'centos' }
+    source TMP_FILE
   end
 
   template File.join(node[:rs_utils][:collectd_plugin_dir], 'mysql.conf') do
     backup false
     source "mysql_collectd_plugin.conf.erb"
     notifies :restart, resources(:service => "collectd")
-    only_if { node[:platform] == 'centos' }
   end
   
 else
