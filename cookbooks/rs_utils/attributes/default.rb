ec37eff5e1f0281fe0fd1f84be4afa7b5b0dc737
kbockmanrs <kevin@rightscale.com>
2011-07-23 00:22:11 +0000

    [rs_utils] Add collectd_share attribute

--- cookbooks/rs_utils/attributes/default.rb
+++ cookbooks/rs_utils/attributes/default.rb
@@ -33,6 +33,7 @@ set_unless[:rs_utils][:timezone] = "UTC"
 set_unless[:rs_utils][:process_list] = ""
 set_unless[:rs_utils][:process_match_list] = ""   
 set_unless[:rs_utils][:private_ssh_key] = ""
+set_unless[:rs_utils][:collectd_share] = "/usr/share/collectd"
 
 set_unless[:rs_utils][:mysql_binary_backup_file] = "/var/run/mysql-binary-backup"
 
