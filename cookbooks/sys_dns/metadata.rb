1cce606d774e62db856a5c610665804f822a129e
Douglas Thrit <douglas.thrift@rightscale.com>
2011-08-02 21:53:27 +0000

    [sys_dns] Initial DNS chef recipe.

---
+++ cookbooks/sys_dns/metadata.rb
@@ -0,0 +1,30 @@
+maintainer       "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures sys_dns"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
+
+recipe "sys_dns::default", "Verifies rightscale_tools installed."
+recipe "sys_dns::do_set_private", "Sets the dynamic DNS entry to the first private IP of the server."
+
+attribute "sys_dns/provider",
+  :display_name => "Provider",
+  :description => "TODO",
+  :required => "required",
+  :choice => ["DNSMadeEasy", "DynDNS", "Route53"]
+
+attribute "sys_dns/id",
+  :display_name => "Dynamic Record ID",
+  :description => "TODO",
+  :required => "required"
+
+attribute "sys_dns/user",
+  :display_name => "User",
+  :description => "TODO",
+  :required => "required"
+
+attribute "sys_dns/password",
+  :display_name => "Password",
+  :description => "TODO",
+  :required => "required"
