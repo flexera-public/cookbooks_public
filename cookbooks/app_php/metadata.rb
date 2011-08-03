b0a790704d882be982545c891f44df8d14c337fa
kbockmanrs <kevin@rightscale.com>
2011-08-03 17:48:54 +0000

    [app_php] Remove depends on repo_git_pull definition

--- cookbooks/app_php/metadata.rb
+++ cookbooks/app_php/metadata.rb
@@ -8,7 +8,6 @@ version          "0.0.1"
 depends "web_apache"
 depends "db_mysql"
 depends "repo_git"
-depends "repo_git_pull(url, branch, dest, cred)"
  
 recipe  "app_php::default", "Installs the php application server."
 recipe  "app_php::do_update_code", "Update application source files from the remote repository."
