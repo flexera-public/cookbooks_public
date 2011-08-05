ce2cbe50e5d473b82be501a9dbc18c04525c199b
Lopaka Delp <lopaka@rightscale.com>
2011-08-05 16:28:25 -0700

    [db_mysql::setup_continuous_backups_cloud_files setup_continuous_backups_ebs setup_continuous_backups_s3] w3903 wrong recipe added to cron

--- cookbooks/db_mysql/recipes/setup_continuous_backups_s3.rb
+++ cookbooks/db_mysql/recipes/setup_continuous_backups_s3.rb
@@ -7,6 +7,6 @@ cron "RightScale continuous backups S3" do
   minute "#{node[:db_mysql][:backup][:cron_backup_minute]}"
   hour "#{node[:db_mysql][:backup][:cron_backup_hour]}"
   user "root"
-  command "rs_run_recipe -n \"block_device::do_backup_s3\" 2>&1 > /var/log/rightscale_tools_cron_backup.log"
+  command "rs_run_recipe -n \"db_mysql::do_backup_s3\" 2>&1 > /var/log/rightscale_tools_cron_backup.log"
   action :create
 end
