1b3dc69af4d0da47ab6938e3e9c1ee3b79e8f9dd
Erik Kargard <erik@rightscale.com>
2011-08-10 17:32:05 +0000

    [db_mysql] More tuning adjustments

--- cookbooks/db_mysql/attributes/mysql_tuning.rb
+++ cookbooks/db_mysql/attributes/mysql_tuning.rb
@@ -23,7 +23,7 @@
 
 # Ohai returns total in KB.  Set GB so X*gb can be used in conditional
 gb=1024*1024
-mem = memory[:total]
+mem = memory[:total].to_i
 Chef::Log.info("Auto-tuning MySQL parameters.  Total memory: #{mem}")
 
 set_unless[:db_mysql][:tunable][:thread_cache_size] = "50"
@@ -43,11 +43,11 @@ set_unless[:db_mysql][:tunable][:key_buffer] = "128M"
 # <3gb=256, 3<>10=512, 10<>25=1024, 25<>50=2048, >50=4096
 if mem < 3*gb
   tune = "256"
-else if mem < 10*gb
+elsif mem < 10*gb
   tune = "512"
-else if mem < 25*gb
+elsif mem < 25*gb
   tune = "1024"
-else if mem < 50*gb
+elsif mem < 50*gb
   tune = "2048"
 else 
   tune = "4096"
@@ -57,11 +57,11 @@ set_unless[:db_mysql][:tunable][:table_cache] = tune
 # <3gb=2m, 3<>10=4m, 10<>25=8m, 25<>50=16m, >50=32m
 if mem < 3*gb
   tune = "2m"
-else if mem < 10*gb
+elsif mem < 10*gb
   tune = "4m"
-else if mem < 25*gb
+elsif mem < 25*gb
   tune = "8m"
-else if mem < 50*gb
+elsif mem < 50*gb
   tune = "16m"
 else 
   tune = "32m"
@@ -75,11 +75,11 @@ set_unless[:db_mysql][:tunable][:read_rnd_buffer_size] = "4M"
 #<3gb=64m, 3<>10=96m, 10<>25=128m, 25<>50=256m, >50=512m
 if mem < 3*gb
   tune = "64m"
-else if mem < 10*gb
+elsif mem < 10*gb
   tune = "96m"
-else if mem < 25*gb
+elsif mem < 25*gb
   tune = "128m"
-else if mem < 50*gb
+elsif mem < 50*gb
   tune = "256m"
 else 
   tune = "512m"
@@ -92,11 +92,11 @@ set_unless[:db_mysql][:tunable][:innodb_buffer_pool_size] = mem*0.80
 #<3gb=50m, 3<>10=200m, 10<>25=300m, 25<>50=400m, >50=500m
 if mem < 3*gb
   tune = "50m"
-else if mem < 10*gb
+elsif mem < 10*gb
   tune = "200m"
-else if mem < 25*gb
+elsif mem < 25*gb
   tune = "300m"
-else if mem < 50*gb
+elsif mem < 50*gb
   tune = "400m"
 else 
   tune = "500m"
@@ -114,11 +114,11 @@ set_unless[:db_mysql][:tunable][:myisamchk][:sort_buffer_size] = "128M"
 #<3gb=600, 3<>10=1200, 10<>25=1800, 25<>50=2400, >50=3000
 if mem < 3*gb
   tune = "600"
-else if mem < 10*gb
+elsif mem < 10*gb
   tune = "1200"
-else if mem < 25*gb
+elsif mem < 25*gb
   tune = "1800"
-else if mem < 50*gb
+elsif mem < 50*gb
   tune = "2400"
 else 
   tune = "3000"
