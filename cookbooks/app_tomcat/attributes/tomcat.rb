682d99674b51815c4e8513bc8c5054092c108f45
Lopaka Delp <lopaka@rightscale.com>
2011-08-15 00:25:33 -0700

    [app_tomcat] docroot removed from metadata and added as attribute

--- cookbooks/app_tomcat/attributes/tomcat.rb
+++ cookbooks/app_tomcat/attributes/tomcat.rb
@@ -9,6 +9,7 @@ set_unless[:tomcat][:code][:url] = ""
 set_unless[:tomcat][:code][:credentials] = ""
 set_unless[:tomcat][:code][:branch] = "master"  
 set_unless[:tomcat][:db_adapter] = "mysql"
+set_unless[:tomcat][:docroot] = "/usr/share/tomcat6/webapps/ROOT"
 
 # == Calculated attributes
 #
--- cookbooks/app_tomcat/metadata.rb
+++ cookbooks/app_tomcat/metadata.rb
@@ -26,13 +26,6 @@ attribute "tomcat/db_name",
   :required => true,
   :recipes => [ "app_tomcat::setup_db_connection"  ]
 
-attribute "tomcat/docroot",
-  :display_name => "Document Root",
-  :description => "Path of the directory from which tomcat will serve files",
-  :required => false,
-  :default => "/usr/share/tomcat6/webapps/ROOT/test",
-  :recipes => [ "app_tomcat::do_update_code"  ]
-
 attribute "tomcat/code",
   :display_name => "Tomcat Application Code",
   :type => "hash"
