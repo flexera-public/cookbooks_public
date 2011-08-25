a301e551e3bb51e55a559fbdf23ea22c29cc8b1b
caryp <cary@rightscale.com>
2011-08-25 15:32:58 -0700

    [db] add snapshot action for secondary backup

--- cookbooks/db/recipes/do_secondary_backup.rb
+++ cookbooks/db/recipes/do_secondary_backup.rb
@@ -8,6 +8,19 @@ db DATA_DIR do
   action [ :pre_backup_check, :lock ]
 end
 
+log "  Performing Snapshot..."
+# Requires block_device node[:db][:block_device] to be instantiated
+# previously. Make sure block_device::default recipe has been run.
+block_device DATA_DIR do
+  lineage node[:db][:backup][:lineage]
+  action :snapshot
+end
+
+log "  Performing unlock DB..."
+db DATA_DIR do
+  action :unlock
+end
+
 log "  Performing Backup..."
 # Requires block_device DATA_DIR to be instantiated
 # previously. Make sure block_device::default recipe has been run.
@@ -20,9 +33,9 @@ block_device DATA_DIR do
   action :backup
 end
 
-log "  Performing unlock DB, the perform post-backup cleanup..."
+log "  Performing post-backup cleanup..."
 db DATA_DIR do
-  action [ :unlock, :post_backup_cleanup ]
+  action :post_backup_cleanup
 end
 
 rs_utils_marker :end
