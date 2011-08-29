e7a3eeff4f50c4b0a4672db9adbe3c7909e88665
caryp <cary@rightscale.com>
2011-08-29 15:27:49 -0700

    [block_device] added as a placeholder to meet the block_device dependancy requirement for public RightScale cookbooks

---
+++ cookbooks/block_device/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures block_device"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
