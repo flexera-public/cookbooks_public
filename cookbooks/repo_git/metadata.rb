c6a93e1192cf4e47f48deee6c323283fe6d79bbc
Dean Onishi <dean@rightscale.com>
2011-09-07 16:48:25 -0700

    Updated recipe description.

--- cookbooks/repo_git/metadata.rb
+++ cookbooks/repo_git/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
 license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
-description      "Installs the git fast version control system"
+description      "Installs Git's Fast Version Control System."
 version          "0.0.1"
 
 depends "git"
