a895226dc8f51fcd73a1296226d263f4148c8adb
Dean Onishi <dean@rightscale.com>
2011-09-07 17:32:35 -0700

    Updated recipe and input descriptions.

--- cookbooks/sys/metadata.rb
+++ cookbooks/sys/metadata.rb
@@ -9,18 +9,18 @@ depends "rs_utils"
 
 recipe "sys::do_reconverge_list_enable", "Enable list of recipes to run every 15 minutes."
 recipe "sys::do_reconverge_list_disable", "Disable recipe reconverge list."
-recipe "sys::setup_swap", "Install swap space."
+recipe "sys::setup_swap", "Installs swap space."
 
 attribute "sys/reconverge_list",
   :display_name => "Reconverge List",
-  :description => "A space-separated list of recipes to run every 15 minutes.  This is used to enforce system consistency.  Ex: app::do_firewall_request_open lb_haproxy::do_attach_all",
+  :description => "A space-separated list of recipes to run every 15 minutes, which is designed to enforce system consistency.  Ex: app::do_firewall_request_open lb_haproxy::do_attach_all",
   :required => "optional",
   :default => "",
   :recipes => [ "sys::default", "sys::do_reconverge_list_enable", "sys::do_reconverge_list_disable" ]
 
 attribute "sys/swap_size",
   :display_name => "Swap size in GB",
-  :description => "Create and activate swap file.  Select '0' to disable swap. ex: 0.5, 1, 1.5",
+  :description => "Creates and activates a swap file based on the selected size (in GB).  Select '0' to disable swap. Ex: 0.5, 1, 1.5",
   :type => "string",
   :choice => ["0","0.5","2.0"],
   :required => true,
@@ -28,7 +28,7 @@ attribute "sys/swap_size",
 
 attribute "sys/swap_file",
   :display_name => "Swapfile location",
-  :description => "Location of swapfile.  Defaults to '/swapfile'.",
+  :description => "The location of swap file.  Defaults to '/swapfile'.",
   :type => "string",
   :default => "/swapfile",
   :recipes => [ "sys::setup_swap"]
