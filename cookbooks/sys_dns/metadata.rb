a3e447bfb4f588b111b6e7cffd49b65d40b9b86d
Erik Kargard <erik@rightscale.com>
2011-09-10 00:11:51 +0000

    [sys_dns] Added default dependencies for DNS attributes

--- cookbooks/sys_dns/metadata.rb
+++ cookbooks/sys_dns/metadata.rb
@@ -13,22 +13,22 @@ attribute "sys_dns/choice",
   :description => "The name of your DNS provider.  Select the DNS provider that you're using to manage the DNS A Records of your Master/Slave DB servers.   Ex: DNSMadeEasy, DynDNS, Route53",
   :required => "required",
   :choice => ["DNSMadeEasy", "DynDNS", "Route53"],
-  :recipes => ["sys_dns::do_set_private"]
+  :recipes => ["sys_dns::do_set_private", "sys_dns::default"]
 
 attribute "sys_dns/id",
   :display_name => "DNS Record ID",
   :description => "The unique identifier that is associated with the DNS A Record of the Master-DB.  The unique identifier is assigned by the DNS provider when you create a dynamic DNS A Record.   This ID is used to update the associated A Record with the private IP Address of the Master-DB when defining which server is the master database.  If you are using DNSMadeEasy as your DNS provider, a 7-digit number is used.  (Ex: 4403234)",
   :required => "required",
-  :recipes => ["sys_dns::do_set_private"]
+  :recipes => ["sys_dns::do_set_private", "sys_dns::default"]
 
 attribute "sys_dns/user",
   :display_name => "DNS User",
   :description => "The username that is used to access and modify your DNS A Records. For DNSMadeEasy and DynDNS, enter your username. (Ex: myUsername)  For AwsDNS, enter your AWS Access Key ID.  (Ex: 1JHQQ4KVEVM1JHQQ4KVE)",
   :required => "required",
-  :recipes => ["sys_dns::do_set_private"]
+  :recipes => ["sys_dns::do_set_private", "sys_dns::default"]
 
 attribute "sys_dns/password",
   :display_name => "DNS Password",
   :description => "The password that is used to access and modify your DNS A Records. For DNSMadeEasy and DynDNS, enter your password. (Ex: myPassw0rd)  For AwsDNS, enter your AWS Secret Access Key. (Ex: XVdxPgOM4auGcMlPz61XVdxPgOM4auGcMlPz6)",
   :required => "required",
-  :recipes => ["sys_dns::do_set_private"]
+  :recipes => ["sys_dns::do_set_private", "sys_dns::default"]
