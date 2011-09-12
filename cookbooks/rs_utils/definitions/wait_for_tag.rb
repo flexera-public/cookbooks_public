414811574536ac03fdc03472027d23625c1409fc
Erik Kargard <erik@rightscale.com>
2011-09-12 19:41:44 +0000

    [rs_utils] Massaged wait_for_tag into possibly working.

--- cookbooks/rs_utils/definitions/wait_for_tag.rb
+++ cookbooks/rs_utils/definitions/wait_for_tag.rb
@@ -8,19 +8,20 @@ define :wait_for_tag, :collection_name => nil, :should_exist => true, :timeout =
   should_exist = params[:should_exist]
   state = should_exist ? "exist" : "vanish"
 
+  server_collection_resource = server_collection collection do
+    tags tag
+    action :nothing
+  end
+
   ruby_block "wait for tag #{state}." do
     block do
       require 'timeout'
     
-      resrc = Chef::Resource::ServerCollection.new(collection)
-      resrc.tags tag
-      provider = Chef::Provider::ServerCollection.new(node, resrc)
-      
       done = false
       begin
         status = Timeout::timeout(timeout) do
           until done
-            provider.send("action_load")
+            server_collection_resource.run_action("load")
             h = node[:server_collection][collection]
             tags = h[h.keys[0]]
             done = true if (tags && should_exist || tags == nil && !should_exist)
