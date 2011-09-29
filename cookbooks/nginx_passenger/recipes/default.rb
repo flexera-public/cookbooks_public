e1a82bf8c2a83a5452d63720a091cb633521cb35
Richard Shade <rshade@rightscale.com>
2011-09-29 09:15:46 -0500

    updated

--- cookbooks/nginx_passenger/recipes/default.rb
+++ cookbooks/nginx_passenger/recipes/default.rb
@@ -5,16 +5,20 @@
 
 case node[:platform]
 when "ubuntu","debian"
-	bash "apt-install" do 
-		code<<-EOF
+	bash "apt-install" do
+		user "root"
+  	cwd "/tmp"
+		code <<-EOF
 		echo "deb http://apt.brightbox.net hardy main" >> /etc/apt/sources.list.d/passenger
 		apt-get update
 		apt-get install nginx-brightbox
 		EOF
 	end
 when "centos","rhel"
-	bash "yum install" do 
-		code<<-EOF
+	bash "yuminstall" do
+		user "root"
+    cwd "/tmp"
+		code <<-EOF
 		rpm -Uvh http://passenger.stealthymonkeys.com/rhel/5/passenger-release.noarch.rpm
 		yum install -y nginx-passenger
 		EOF
