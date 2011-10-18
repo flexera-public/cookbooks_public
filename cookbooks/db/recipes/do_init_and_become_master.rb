0a4525830c9c87eac687f69de4996a417d0b05df
Lopaka Delp <lopaka@rightscale.com>
2011-10-17 17:41:42 -0700

    [db::do_init_and_become_master] initial commit for new recipe

---
+++ cookbooks/db/recipes/do_init_and_become_master.rb
@@ -0,0 +1,39 @@
+# Cookbook Name:: db
+#
+# Copyright (c) 2011 RightScale Inc
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+rs_utils_marker :begin
+
+include_recipe "db::setup_block_device"
+include_recipe "db::do_tag_as_master"
+include_recipe "db::setup_replication_privileges"
+# kick-off first backup so that slaves can init from this master
+include_recipe "db::do_backup"
+include_recipe "db::do_backup_schedule_enable"
+
+ruby_block "Setting db_restored state to true" do
+  block do
+    node[:db][:db_restored] = true
+  end
+end
+
+rs_utils_marker :end
