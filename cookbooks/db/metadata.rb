c23cf591908956ef33d5dc9890ac3def39c93f3f
Lopaka Delp <lopaka@rightscale.com>
2011-10-21 14:52:01 -0700

    [db] added db::do_dump_import to metadata

--- cookbooks/db/metadata.rb
+++ cookbooks/db/metadata.rb
@@ -38,7 +38,8 @@ recipe  "db::do_secondary_restore", "Restores the database from the most recentl
 
 recipe  "db::do_force_reset", "Resets the database back to a pristine state. WARNING: Execution of this script will delete any data in your database!"
 
-recipe  "db::do_dump_export", "Creates a mysql dump file and uploads it to an ROS."
+recipe  "db::do_dump_export", "Creates a dump file and uploads it to an ROS."
+recipe  "db::do_dump_import", "Retrieves a dump file from ROS and imports it into DB."
 
 
 # == Database Firewall Recipes
