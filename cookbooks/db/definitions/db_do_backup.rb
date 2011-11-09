248fac98b684d2764424bf837fd2d73f84bd3d2d
Lopaka Delp <lopaka@rightscale.com>
2011-11-09 09:08:52 -0800

    [db] definitions/db_do_backup - fixed typo in command sent to backup.rb

--- cookbooks/db/definitions/db_do_backup.rb
+++ cookbooks/db/definitions/db_do_backup.rb
@@ -113,7 +113,7 @@ define :db_do_backup, :force => false, :backup_type => "primary" do
       'STORAGE_ACCOUNT_SECRET_AWS' => node[:block_device][:aws_secret_access_key]
     })
     code <<-EOH
-    /opt/rightscale/sandbox/bin/backup.rb --backuponly --lineage #{node[:db][:backup][:lineage]} --cloud #{node[:cloud][:provider]} #{storage_cloud ? '--storage_cloud ' + storage_cloud : ''} --storage-type #{storage_type} #{node[:block_device][:rackspace_snet] ? '' : '--no-snet'} --container #{storage_container} 2>&1 | logger -t rs_db_backup &
+    /opt/rightscale/sandbox/bin/backup.rb --backuponly --lineage #{node[:db][:backup][:lineage]} --cloud #{node[:cloud][:provider]} #{storage_cloud ? '--storage-cloud ' + storage_cloud : ''} --storage-type #{storage_type} #{node[:block_device][:rackspace_snet] ? '' : '--no-snet'} --container #{storage_container} 2>&1 | logger -t rs_db_backup &
     EOH
   end
 
