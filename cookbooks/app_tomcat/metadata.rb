82047706009884c01e97f3e01218571af60bda69
Erik Kargard <erik@rightscale.com>
2011-12-01 07:51:41 +0000

    [ALL] Update metadata license

--- cookbooks/app_tomcat/metadata.rb
+++ cookbooks/app_tomcat/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
-#license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
+license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
 description      "Installs the tomcat application server."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
--- cookbooks/repo/metadata.rb
+++ cookbooks/repo/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
-license          "All rights reserved"
+license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
 description      "Abstract Repository Cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
