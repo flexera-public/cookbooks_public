531066f02b2e5318013a33b1270d275531feb402
Lopaka Delp <lopaka@rightscale.com>
2011-12-02 14:18:50 -0800

    [db_mysql] providers/default.rb - re-added needed include line

--- cookbooks/db_mysql/providers/default.rb
+++ cookbooks/db_mysql/providers/default.rb
@@ -5,6 +5,8 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
+include RightScale::Database::MySQL::Helper
+
 action :stop do
   @db = init(new_resource)
   @db.stop
