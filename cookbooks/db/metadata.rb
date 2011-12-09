84d5925b9edddbccfdb0e39b4a875ece180d7239
Erik Kargard <erik@rightscale.com>
2011-12-09 01:24:27 +0000

    [db]Inputs re-org

--- cookbooks/db/metadata.rb
+++ cookbooks/db/metadata.rb
@@ -21,11 +21,11 @@ recipe  "db::setup_monitoring", "Installs the collectd plugin for database monit
 
 # == Common Database Recipes
 #
-recipe  "db::do_backup", "Creates a backup of the database using persistent storage in the current cloud.  On Rackspace, LVM backups are uploaded to the specified CloudFiles container.  For all other clouds, volume snapshots (like EBS) are used."
-recipe  "db::do_restore", "Restores the database from the most recently completed backup available in persistent storage of the current cloud."
+recipe  "db::do_primary_backup", "Creates a primary backup of the database using persistent storage in the current cloud.  On Rackspace, LVM backups are uploaded to the specified CloudFiles container.  For all other clouds, volume snapshots (like EBS) are used."
+recipe  "db::do_primary_restore", "Restores the database from the most recently completed primary backup available in persistent storage of the current cloud."
 
-recipe "db::do_backup_schedule_enable", "Enables db::do_backup to be run periodically."
-recipe "db::do_backup_schedule_disable", "Disables db::do_backup from being run periodically."
+recipe "db::do_primary_backup_schedule_enable", "Enables db::do_primary_backup to be run periodically."
+recipe "db::do_primary_backup_schedule_disable", "Disables db::do_primary_backup from being run periodically."
 
 recipe  "db::setup_privileges_admin", "Adds the username and password for 'superuser' privileges."
 recipe  "db::setup_privileges_application", "Adds the username and password for application privileges."
@@ -55,7 +55,7 @@ recipe "db::request_appserver_deny", "Sends a request to deny connections from t
 # == Master/Slave Recipes
 #
 recipe "db::do_init_and_become_master", "Initializes MySQL database.  Tag as Master.  Set Master DNS.  Kick off a fresh backup from this master."
-recipe "db::do_restore_and_become_master", "Restore MySQL database.  Tag as Master.  Set Master DNS.  Kick off a fresh backup from this master."
+recipe "db::do_primary_restore_and_become_master", "Restore MySQL database.  Tag as Master.  Set Master DNS.  Kick off a fresh backup from this master."
 recipe "db::do_init_slave", "Initialize MySQL Slave"
 recipe "db::do_init_slave_at_boot", "Initialize MySQL Slave at boot."
 recipe "db::do_promote_to_master", "Promote a replicating slave to master"
@@ -95,13 +95,13 @@ attribute "db/replication/user",
   :display_name => "Database Replication Username",
   :description => "The username of the database user that has 'replciation' privileges.",
   :required => true,
-  :recipes => [ "db::setup_replication_privileges", "db::do_restore_and_become_master", "db::do_init_and_become_master", "db::do_promote_to_master", "db::do_init_slave", "db::do_init_slave_at_boot" ]
+  :recipes => [ "db::setup_replication_privileges", "db::do_primary_restore_and_become_master", "db::do_init_and_become_master", "db::do_promote_to_master", "db::do_init_slave", "db::do_init_slave_at_boot" ]
 
 attribute "db/replication/password",
   :display_name => "Database Replication Password",
   :description => "The password of the database user that has 'replciation' privileges.",
   :required => true,
-  :recipes => [ "db::setup_replication_privileges", "db::do_restore_and_become_master", "db::do_init_and_become_master", "db::do_promote_to_master","db::do_init_slave", "db::do_init_slave_at_boot" ]
+  :recipes => [ "db::setup_replication_privileges", "db::do_primary_restore_and_become_master", "db::do_init_and_become_master", "db::do_promote_to_master","db::do_init_slave", "db::do_init_slave_at_boot" ]
 
 attribute "db/application/user",
   :display_name => "Database Application Username",
@@ -133,12 +133,12 @@ attribute "db/backup/lineage",
     "db::do_init_slave",
     "db::do_init_slave_at_boot",
     "db::do_promote_to_master",
-    "db::do_restore_and_become_master",
+    "db::do_primary_restore_and_become_master",
     "db::do_init_and_become_master",
-    "db::do_backup",
-    "db::do_restore",
-    "db::do_backup_schedule_enable",
-    "db::do_backup_schedule_disable",
+    "db::do_primary_backup",
+    "db::do_primary_restore",
+    "db::do_primary_backup_schedule_enable",
+    "db::do_primary_backup_schedule_disable",
     "db::do_force_reset",
     "db::do_secondary_backup",
     "db::do_secondary_restore"
@@ -148,44 +148,44 @@ attribute "db/backup/timestamp_override",
   :display_name => "Restore Timestamp Override", 
   :description => "An optional variable to restore from a specific timestamp. You must specify a string that matches the timestamp tag on the volume snapshot.  You will need to specify the timestamp that's defined by the snapshot's tag (not the name).  For example, if the snapshot's tag is 'rs_backup:timestamp=1303613371' you would specify '1303613371' for this input.",
   :required => false,
-  :recipes => [ "db::do_restore", "db::do_secondary_restore" ]
+  :recipes => [ "db::do_primary_restore", "db::do_secondary_restore" ]
   
-attribute "db/backup/secondary_location",
-  :display_name => "Secondary Backup Location",
+attribute "db/backup/secondary/cloud",
+  :display_name => "Secondary Backup Cloud",
   :description => "Location for secondary backups. Used by db::do_secondary_backup and db::do_secondary_restore to backup to persistent storage outside of the current cloud. For example, you can specify the name of an AWS S3 bucket or Rackspace CloudFiles container.",
   :default => "S3",
   :choice => [ "S3", "CloudFiles" ],
   :recipes => [ "db::do_secondary_backup", "db::do_secondary_restore" ]
 
-attribute "db/backup/secondary_container",
+attribute "db/backup/secondary/container",
   :display_name => "Secondary Backup Container",
   :description => "Container for secondary backups. Used by db::do_secondary_backup and db::do_secondary_restore to backup to persistent storage outside of the current cloud. For example, you can specify the name of an AWS S3 bucket or Rackspace CloudFiles container.",
   :required => true,
   :recipes => [ "db::do_secondary_backup", "db::do_secondary_restore" ]
 
-attribute "db/backup/master/hour",
+attribute "db/backup/primary/master/cron/hour",
   :display_name => "Master Backup Cron Hour",
   :description => "Defines the hour of the day when the backup EBS snapshot will be taken of the Master database.  Backups of the Master are taken daily.  By default, an hour will be randomly chosen at launch time.  The time of the backup is defined by 'Master Backup Cron Hour' and 'Master Backup Cron Minute'.  Uses standard crontab format. (Ex: 23) for 11pm.",
   :required => false,
-  :recipes => [ 'db::do_backup_schedule_enable' ]
+  :recipes => [ 'db::do_primary_backup_schedule_enable' ]
 
-attribute "db/backup/slave/hour",
+attribute "db/backup/primary/slave/cron/hour",
   :display_name => "Slave Backup Cron Hour",
   :description => "By default, backup EBS Snapshots of the Slave database are taken hourly. (Ex: *)",
   :required => false,
-  :recipes => [ 'db::do_backup_schedule_enable' ]
+  :recipes => [ 'db::do_primary_backup_schedule_enable' ]
 
-attribute "db/backup/master/minute",
+attribute "db/backup/primary/master/cron/minute",
   :display_name => "Master Backup Cron Minute",
   :description => "Defines the minute of the hour when the backup EBS snapshot will be taken of the Master database.  Backups of the Master are taken daily.  By default, a minute will be randomly chosen at launch time.  The time of the backup is defined by 'Master Backup Cron Hour' and 'Master Backup Cron Minute'.  Uses standard crontab format. (Ex: 30) for the 30th minute of the hour.",
   :required => false,
-  :recipes => [ 'db::do_backup_schedule_enable' ]
+  :recipes => [ 'db::do_primary_backup_schedule_enable' ]
 
-attribute "db/backup/slave/minute",
+attribute "db/backup/primary/slave/cron/minute",
   :display_name => "Slave Backup Cron Minute",
   :description => "Defines the minute of the hour when the backup EBS snapshot will be taken of the Slave database.  Backups of the Slave are taken hourly.  By default, a minute will be randomly chosen at launch time.  Uses standard crontab format. (Ex: 30) for the 30th minute of the hour.",
   :required => false,
-  :recipes => [ 'db::do_backup_schedule_enable' ]
+  :recipes => [ 'db::do_primary_backup_schedule_enable' ]
 
 
 # == Import/export attributes
