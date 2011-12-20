f3264f7246e216ec46c5ca36b1f7ff0899a52433
sarahrs <sarah.lowe@rightscale.com>
2011-12-20 15:03:19 -0800

    Update cookbooks/sys_dns/metadata.rb

--- cookbooks/sys_dns/metadata.rb
+++ cookbooks/sys_dns/metadata.rb
@@ -12,20 +12,20 @@ recipe "sys_dns::do_set_private", "Sets the dynamic DNS entry to the first priva
 
 attribute "sys_dns/choice",
   :display_name => "DNS Service Provider",
-  :description => "The name of your DNS provider.  Select the DNS provider that you're using to manage the DNS A Records of your Master/Slave DB servers.   Ex: DNSMadeEasy, DynDNS, Route53",
+  :description => "The name of your DNS provider. Select the DNS provider that you're using to manage the DNS A records of your master/slave database servers (e.g., DNSMadeEasy, DynDNS, Route53).",
   :required => "required",
   :choice => ["DNSMadeEasy", "DynDNS", "Route53"],
   :recipes => ["sys_dns::do_set_private", "sys_dns::default"]
 
 attribute "sys_dns/id",
-  :display_name => "DNS Record ID",
-  :description => "The unique identifier that is associated with the DNS A Record of the Master-DB.  The unique identifier is assigned by the DNS provider when you create a dynamic DNS A Record.   This ID is used to update the associated A Record with the private IP Address of the Master-DB when defining which server is the master database.  If you are using DNSMadeEasy as your DNS provider, a 7-digit number is used.  (Ex: 4403234)",
+  :display_name => "Master DNS Record ID",
+  :description => "The unique identifier that is associated with the DNS A record of the master database server.  The unique identifier is assigned by the DNS provider when you create a dynamic DNS A record. This ID is used to update the associated A record with the private IP address of the master server when defining which server is the master.  If you are using DNS Made Easy as your DNS provider, a 7-digit number is used (e.g., 4403234).",
   :required => "required",
   :recipes => ["sys_dns::do_set_private", "sys_dns::default"]
 
 attribute "sys_dns/user",
   :display_name => "DNS User",
-  :description => "The user name that is used to access and modify your DNS A Records. For DNS Made Easy and DynDNS, enter your username (e.g., cred:DNS_USER). For Amazon DNS, enter your Amazon access key ID (e.g., cred:AWS_ACCESS_KEY_ID)",
+  :description => "The user name that is used to access and modify your DNS A records. For DNS Made Easy and DynDNS, enter your user name (e.g., cred:DNS_USER). For Amazon DNS, enter your Amazon access key ID (e.g., cred:AWS_ACCESS_KEY_ID)",
   :required => "required",
   :recipes => ["sys_dns::do_set_private", "sys_dns::default"]
 
