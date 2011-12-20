cb5bbf6cda7a10e10401dfaa2e7ddeefaa49a166
NS <n.staknov@tut.by>
2011-12-20 14:09:46 +0300

    [app_tomcat]add package deps

--- cookbooks/app_tomcat/attributes/tomcat.rb
+++ cookbooks/app_tomcat/attributes/tomcat.rb
@@ -20,16 +20,24 @@ set[:tomcat][:docroot] = "/srv/tomcat6/webapps"
 # == Calculated attributes
 #
 case platform
-when "ubuntu", "debian"
-  set[:db_mysql][:socket] = "/var/run/mysqld/mysqld.sock"
-when "centos","fedora","suse"
-  set[:tomcat][:package_dependencies] = ["eclipse-ecj",\
+  when "ubuntu", "debian"                                       #tomcat6 tomcat6-admin tomcat6-common tomcat6-user
+    set[:tomcat][:package_dependencies] = ["eclipse-ecj",\
+                                         "tomcat6",\
+                                         "tomcat6-admin",\
+                                         "tomcat6-common",\
+                                         "tomcat-user",\
+                                         "mysql-connector-java"]
+    set[:tomcat][:module_dependencies] = [ "proxy", "proxy_http" ]
+    set_unless[:tomcat][:app_user] = "tomcat"
+    set[:db_mysql][:socket] = "/var/run/mysqld/mysqld.sock"
+  when "centos","fedora","suse"
+    set[:tomcat][:package_dependencies] = ["eclipse-ecj",\
                                          "tomcat6",\
                                          "tomcat6-admin-webapps",\
                                          "tomcat6-webapps",\
                                          "tomcat-native",\
                                          "mysql-connector-java"]
-  set[:tomcat][:module_dependencies] = [ "proxy", "proxy_http" ]
-  set_unless[:tomcat][:app_user] = "tomcat"
-  set[:db_mysql][:socket] = "/var/lib/mysql/mysql.sock"
+    set[:tomcat][:module_dependencies] = [ "proxy", "proxy_http" ]
+    set_unless[:tomcat][:app_user] = "tomcat"
+    set[:db_mysql][:socket] = "/var/lib/mysql/mysql.sock"
 end
