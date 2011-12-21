62d42419d0eca91c24e34a5f804655f2c175bc93
Erik Kargard <erik@rightscale.com>
2011-12-21 21:41:43 +0000

    Revert "[db] set this_is_master in the compile phase (w-4278)"
    This reverts commit b7864cb586271055beeb54fff1923395b33fbe0c.

--- cookbooks/db/definitions/db_register_master.rb
+++ cookbooks/db/definitions/db_register_master.rb
@@ -14,20 +14,12 @@
 
 define :db_register_master do
 
-  # == Set master node variables
-  # 
-  # NOTE: this runs in the compile phase since it is needed for backup
-  # schedule calculations.
-  #
-  node[:db][:current_master_uuid] = node[:rightscale][:instance_uuid]
-  node[:db][:current_master_ip] = node[:cloud][:private_ips][0]
-  node[:db][:this_is_master] = true
-
   # == Set master DNS
   # Do this first so that DNS can propagate while the recipe runs
   #
   include_recipe "sys_dns::do_set_private"
 
+  
   # == Set master tags
   # Tag the server with the master tags rs_dbrepl:master_active 
   # and rs_dbrepl:master_instance_uuid
@@ -39,4 +31,14 @@ define :db_register_master do
   unique_tag = "rs_dbrepl:master_instance_uuid=#{node[:rightscale][:instance_uuid]}"
   log "Tagging server with #{unique_tag}"
   right_link_tag unique_tag
+  
+  # == Set master node variables
+  #
+  ruby_block "initialize master state" do
+    block do
+      node[:db][:current_master_uuid] = node[:rightscale][:instance_uuid]
+      node[:db][:current_master_ip] = node[:cloud][:private_ips][0]
+      node[:db][:this_is_master] = true
+    end
+  end
 end
