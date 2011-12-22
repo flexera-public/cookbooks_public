14de52b5651eccf5f6132a9f06b441f41811e303
Erik Kargard <erik@rightscale.com>
2011-12-22 00:18:25 +0000

    [db]Enable cron fix. w4278

--- cookbooks/db/recipes/do_primary_backup_schedule_enable.rb
+++ cookbooks/db/recipes/do_primary_backup_schedule_enable.rb
@@ -16,22 +16,35 @@ db_state_assert :either
 snap_lineage = node[:db][:backup][:lineage]
 raise "ERROR: 'Backup Lineage' required for scheduled process" if snap_lineage.empty?
 
-if node[:db][:this_is_master]
-  hour = node[:db][:backup][:primary][:master][:cron][:hour]
-  minute = node[:db][:backup][:primary][:master][:cron][:minute]
-  type = "Master"
-else
-  hour = node[:db][:backup][:primary][:slave][:cron][:hour]
-  minute = node[:db][:backup][:primary][:slave][:cron][:minute]
-  type = "Slave"
+master_hour = node[:db][:backup][:primary][:master][:cron][:hour].to_s
+master_minute = node[:db][:backup][:primary][:master][:cron][:minute].to_s
+slave_hour = node[:db][:backup][:primary][:slave][:cron][:hour].to_s
+slave_minute = node[:db][:backup][:primary][:slave][:cron][:minute].to_s
+
+log "  Setting up Master primary backup cron job to run at hour: #{master_hour} and minute #{master_minute}" do
+  only_if { node[:db][:this_is_master] }
+end
+block_device NICKNAME do
+  only_if { node[:db][:this_is_master] }
+  lineage snap_lineage
+  cron_backup_recipe "db::do_primary_backup"
+  cron_backup_hour master_hour
+  cron_backup_minute master_minute
+  persist false
+  action :backup_schedule_enable
 end
 
-log "  Setting up #{type} primary backup cron job to run at hour: #{hour} and minute #{minute}"
+log "  Setting up Slave primary backup cron job to run at hour: #{slave_hour} and minute #{slave_minute}" do
+  not_if { node[:db][:this_is_master] }
+end
 block_device NICKNAME do
+  only_if { true }
+  not_if { node[:db][:this_is_master] }
   lineage snap_lineage
-  cron_backup_recipe "#{self.cookbook_name}::do_primary_backup"
-  cron_backup_hour hour.to_s
-  cron_backup_minute minute.to_s
+  cron_backup_recipe "db::do_primary_backup"
+  cron_backup_hour slave_hour
+  cron_backup_minute slave_minute
+  persist false
   action :backup_schedule_enable
 end
 
