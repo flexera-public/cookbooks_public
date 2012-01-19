c2176679b0f20efad5b349bce6eb601d7e242561
Anand <anandu@gmail.com>
2012-01-19 16:24:11 +0530

    Using provider action "firwall update"

--- cookbooks/db/recipes/do_promote_to_master.rb
+++ cookbooks/db/recipes/do_promote_to_master.rb
@@ -14,11 +14,10 @@ db_state_assert :slave
 
 # == Open port for slave replication by old-master
 #
-sys_firewall "Open port 3306 to the old master which is becoming a slave" do
-  port 3306
+db node[:db][:data_dir] do
   enable true
   ip_addr node[:db][:current_master_ip]
-  action :update
+  action :firewall_update_request
 end
 
 # == Promote to master
