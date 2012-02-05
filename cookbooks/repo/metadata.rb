0419e9a359489e3e07e7b03cf381c087de810605
NS <n.staknov@tut.by>
2012-02-05 19:08:47 +0300

    repo metadata update

--- cookbooks/repo/metadata.rb
+++ cookbooks/repo/metadata.rb
@@ -19,19 +19,19 @@ attribute "repo/default/provider",
   :required => "recommended",
   :choice => ["repo_git", "repo_svn", "repo_ros"],
   :default => "repo_git",
-  :recipes => ["repo::default", "repo::do_pull"]
+  :recipes => ["repo::default"]
 
 attribute "repo/default/repository",
   :display_name => "Repository Url",
   :description => "",
   :required => "recommended",
-  :recipes => ["repo::default", "repo::do_pull"]
+  :recipes => ["repo::default"]
 
 attribute "repo/default/revision",
   :display_name => "Branch/Tag",
   :description => "",
   :required => "recommended",
-  :recipes => ["repo::default", "repo::do_pull"]
+  :recipes => ["repo::default"]
 
 #SVN
 attribute "repo/default/svn_username",
@@ -39,14 +39,14 @@ attribute "repo/default/svn_username",
   :description => "Username for SVN repo",
   :required => "optional",
   :default => "",
-  :recipes => ["repo::default", "repo::do_pull"]
+  :recipes => ["repo::default"]
 
 attribute "repo/default/svn_password",
   :display_name => "SVN password",
   :description => "Password for SVN repo",
   :required => "optional",
   :default => "",
-  :recipes => ["repo::default", "repo::do_pull"]
+  :recipes => ["repo::default"]
 
 #GIT
 attribute "repo/default/ssh_key",
@@ -54,7 +54,7 @@ attribute "repo/default/ssh_key",
   :description => "",
   :default => "",
   :required => "recommended",
-  :recipes => ["repo::default", "repo::do_pull"]
+  :recipes => ["repo::default"]
 
 #ROS
 attribute "repo/default/storage_account_provider",
@@ -62,31 +62,31 @@ attribute "repo/default/storage_account_provider",
   :description => "Location where source file is saved. Used by recipes to upload to Amazon S3 or Rackspace Cloud Files.",
   :required => "optional",
   :choice => [ "S3", "CloudFiles" ],
-  :recipes => ["repo::default", "repo::do_pull"]
+  :recipes => ["repo::default"]
 
 attribute "repo/default/storage_account_id",
   :display_name => "ROS Storage Account ID",
   :description => "In order to write the repo  to the specified cloud storage location, you need to provide cloud authentication credentials. For Amazon S3, use your Amazon access key ID (e.g., cred:AWS_ACCESS_KEY_ID). For Rackspace Cloud Files, use your Rackspace login username (e.g., cred:RACKSPACE_USERNAME).",
   :required => "optional",
-  :recipes => ["repo::default", "repo::do_pull"]
+  :recipes => ["repo::default"]
 
 attribute "repo/default/storage_account_secret",
   :display_name => "ROS Storage Account Secret",
   :description => "In order to write the dump file to the specified cloud storage location, you will need to provide cloud authentication credentials. For Amazon S3, use your AWS secret access key (e.g., cred:AWS_SECRET_ACCESS_KEY). For Rackspace Cloud Files, use your Rackspace account API key (e.g., cred:RACKSPACE_AUTH_KEY).",
   :required => "optional",
-  :recipes => ["repo::default", "repo::do_pull"]
+  :recipes => ["repo::default"]
 
 attribute "repo/default/container",
   :display_name => "ROS Container",
   :description => "The cloud storage location where the dump file will be saved to or restored from. For Amazon S3, use the bucket name. For Rackspace Cloud Files, use the container name.",
   :required => "optional",
-  :recipes => ["repo::default", "repo::do_pull"]
+  :recipes => ["repo::default"]
 
 attribute "repo/default/prefix",
   :display_name => "ROS Prefix",
   :description => "The prefix that will be used to name/locate the backup of a particular source repo. Defines the prefix of the source repo file name that will be used to name the downloaded repo file.",
   :required => "optional",
-  :recipes => ["repo::default", "repo::do_pull"]
+  :recipes => ["repo::default"]
 
 #capistrano attributes used in repo::do_pull
 
