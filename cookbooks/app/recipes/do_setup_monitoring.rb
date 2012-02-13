18f6ff659a5d9c385141b44db5a26f0d593f14db
NS <n.staknov@tut.by>
2012-02-13 16:39:14 +0300

    tomcat: setup_monitoring -> provider
    passenger:
    do_update code ->provider
    
    app
    setup_monitoring  resource init

---
+++ cookbooks/app/recipes/do_setup_monitoring.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name::app
+# Recipe::do_setup_monitoring
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+rs_utils_marker :begin
+
+app "default" do
+    action :setup_monitoring
+end
+
+rs_utils_marker :end
