7ed4fed00026a3d278ac9ba6f5cb6c6357368655
Erik Kargard <erik@rightscale.com>
2012-03-06 02:03:43 +0000

    [repo, repo_ros] Remove direct dependence on rs_tools cookbook. (cherry picked from commit 57db5ccbfd9880231a8b143dd0c54bbece30b35e)

--- cookbooks/repo_ros/metadata.rb
+++ cookbooks/repo_ros/metadata.rb
@@ -1,15 +1,10 @@
 maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
-license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
+license          "Copyright RightScale, Inc. All rights reserved."
 description      "Manages the ROS repo code download"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
 depends "repo"
-depends "rs_tools"
-
-
-
 
 recipe  "repo_ros::default", "Default pattern of loading packages and resources provided"
-
