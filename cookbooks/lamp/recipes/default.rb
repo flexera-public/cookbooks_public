e35bcd19328ca921935fe63dd76167bfb60b9e30
Erik Kargard <erik@rightscale.com>
2012-03-07 20:42:53 +0000

    [lamp]Fixed comment

--- cookbooks/lamp/recipes/default.rb
+++ cookbooks/lamp/recipes/default.rb
@@ -5,7 +5,8 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
-# This recipe performs not actions.  It exists to allow overiding the attributes
+# This recipe performs no actions.  It exists to allow overiding the attributes
 # set in other cookbooks
 rs_utils_marker :begin
+  log "LAMP set to listen on #{node[:db_mysql][:bind_address]}:#{node[:app][:port]}"
 rs_utils_marker :end
