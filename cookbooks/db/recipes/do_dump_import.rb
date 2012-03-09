f3784483855b9b4c57844fa5b46486152eb2b699
Erik Kargard <erik@rightscale.com>
2012-03-09 01:11:59 +0000

    [db]Raise error is skipping import due to invalid prefix.  Skip import for other errors

--- cookbooks/db/recipes/do_dump_import.rb
+++ cookbooks/db/recipes/do_dump_import.rb
@@ -14,10 +14,10 @@ skip, reason = true, "Prefix not provided"                   if node[:db][:dump]
 skip, reason = true, "Storage account provider not provided" if node[:db][:dump][:storage_account_provider] == ""
 skip, reason = true, "Container not provided"                if node[:db][:dump][:container] == ""
 dump_file_regex = '(^\w+)(-\d{1,12})*$'
-skip, reason = true, "Prefix: #{node[:db][:dump][:prefix]} invalid.  It is restricted to word characters (letter, number, underscore) and an optional partial timestamp -YYYYMMDDHHMM.  (=~/#{dump_file_regex}/ is the ruby regex used). ex: myapp_prod_dump, myapp_prod_dump-201203080035 or myapp_prod_dump-201203" unless node[:db][:dump][:prefix] =~ /#{dump_file_regex}/ 
+raise "Prefix: #{node[:db][:dump][:prefix]} invalid.  It is restricted to word characters (letter, number, underscore) and an optional partial timestamp -YYYYMMDDHHMM.  (=~/#{dump_file_regex}/ is the ruby regex used). ex: myapp_prod_dump, myapp_prod_dump-201203080035 or myapp_prod_dump-201203" unless node[:db][:dump][:prefix] =~ /#{dump_file_regex}/ 
 
 if skip
-  raise "Skipping import: #{reason}"
+  log "Skipping import: #{reason}"
 else
 
   db_name      = node[:db][:dump][:database_name]
