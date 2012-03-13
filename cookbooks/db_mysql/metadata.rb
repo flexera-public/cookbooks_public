fe8a4762deeedd9403a328e34dfb16e0881921df
Erik Kargard <erik@rightscale.com>
2012-03-13 01:43:01 +0000

    [db_mysql]Create default server recipe for setting mysql server specific input variables in the node

--- cookbooks/db_mysql/metadata.rb
+++ cookbooks/db_mysql/metadata.rb
@@ -12,6 +12,7 @@ depends "rs_utils"
 
 recipe  "db_mysql::default_5_1", "Set DB MySQL provider, set version 5.1 and node variables specific to MySQL 5.1"
 recipe  "db_mysql::default_5_5", "Set DB MySQL provider, set version 5.5 and node variables specific to MySQL 5.5"
+recipe  "db_mysql::default_server", "Set DB MySQL server specfic input variables"
 
 attribute "db_mysql",
   :display_name => "General Database Options",
@@ -22,7 +23,7 @@ attribute "db_mysql",
 attribute "db_mysql/server_usage",
   :display_name => "Server Usage",
   :description => "Use 'dedicated' if the mysql config file allocates all existing resources of the machine.  Use 'shared' if the MySQL config file is configured to use less resources so that it can be run concurrently with other apps like Apache and Rails for example.",
-  :recipes => [ "db::install_server" ],
+  :recipes => [ "db_mysql::default_server" ],
   :choice => ["shared", "dedicated"],
   :required => "optional",
   :default => "dedicated"
@@ -30,8 +31,7 @@ attribute "db_mysql/server_usage",
 attribute "db_mysql/log_bin",
   :display_name => "MySQL Binlog Destination",
   :description => "Defines the filename and location of your MySQL stored binlog files.  This sets the log-bin variable in the MySQL config file.  If you do not specify an absolute path, it will be relative to the data directory. Ex: /mnt/mysql-binlogs/mysql-bin",
-  :recipes => [ "db::install_server" ],
+  :recipes => [ "db_mysql::default_server" ],
   :required => "optional",
   :default => "/mnt/mysql-binlogs/mysql-bin"
-
 
---
+++ cookbooks/db_mysql/recipes/default_server.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: db_mysql
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+# Recipe to setup dependencies for server (as opposed to client) specific MySQL input
+# variables.
+# This recipe adds server specific input variables to the node for use by other recipes.
+
+rs_utils_marker :begin
+
+rs_utils_marker :end
