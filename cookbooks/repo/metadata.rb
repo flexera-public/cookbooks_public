6a0aa887d07334fb8641e0e72be0e51742f4e6b3
caryp <cary@rightscale.com>
2012-03-27 13:36:24 -0700

    [repo] fixed dependency issue with rs_utils (w-4476)

--- cookbooks/repo/metadata.rb
+++ cookbooks/repo/metadata.rb
@@ -6,6 +6,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 
 version          "0.0.1"
 
+depends "rs_utils"
 depends "repo_svn"
 depends "repo_git"
 depends "repo_ros"
