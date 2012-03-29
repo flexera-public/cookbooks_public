9485a709a66af049cd131c8338ce4c8ea79034a2
Ryan OLeary <ryan.oleary@rightscale.com>
2012-03-29 11:15:04 -0700

    Updated log message on failure to provide more information

--- cookbooks/db/recipes/install_server.rb
+++ cookbooks/db/recipes/install_server.rb
@@ -33,7 +33,7 @@ ruby_block "Master DNS TTL Check" do
 
     dnsttl=`dig #{MASTER_DB_DNSNAME} | grep ^#{MASTER_DB_DNSNAME} | awk '{ print $2}'`
     if dnsttl.to_i > OPT_DNS_TTL_LIMIT.to_i
-       raise "Master DB DNS TTL set to high.  Must be set <= #{OPT_DNS_TTL_LIMIT}"
+       raise "Master DB DNS TTL set to high.  Must be set <= #{OPT_DNS_TTL_LIMIT}. Found #{dnsttl} for #{MASTER_DB_DNSNAME}"
     end
     Chef::Log.info("Pass: Master DB DNS TTL: #{dnsttl}")
   end
