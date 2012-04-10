46c2aaf69f327fe39436cfce4aeda24d503765c8
Erik Kargard <erik@rightscale.com>
2012-04-10 22:11:49 +0000

    [db_mysql]Changed variable recipe dependencies from default_server to install_server.  Added tmpdir input.

--- cookbooks/db_mysql/metadata.rb
+++ cookbooks/db_mysql/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
 license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
-description      "Installs/configures a MySQL database server with automated backups."
+description      "Installs/configures a MySQL database client and server."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.2"
 
@@ -13,7 +13,7 @@ depends "rs_utils"
 recipe  "db_mysql::default", "Set DB MySQL provider, set version and node variables specific to the chosen MySQL version"
 recipe  "db_mysql::default_5_1", "Set DB MySQL provider, set version 5.1 and node variables specific to MySQL 5.1"
 recipe  "db_mysql::default_5_5", "Set DB MySQL provider, set version 5.5 and node variables specific to MySQL 5.5"
-recipe  "db_mysql::default_server", "Set DB MySQL server specfic input variables"
+#recipe  "db_mysql::default_server", "Set DB MySQL server specfic input variables"
 
 attribute "db_mysql",
   :display_name => "General Database Options",
@@ -33,7 +33,7 @@ attribute "db_mysql/version",
 attribute "db_mysql/server_usage",
   :display_name => "Server Usage",
   :description => "Use 'dedicated' if the mysql config file allocates all existing resources of the machine.  Use 'shared' if the MySQL config file is configured to use less resources so that it can be run concurrently with other apps like Apache and Rails for example.",
-  :recipes => ["db_mysql::default_server"],
+  :recipes => ["db_mysql::install_server"],
   :choice => ["shared", "dedicated"],
   :required => "optional",
   :default => "dedicated"
@@ -41,7 +41,14 @@ attribute "db_mysql/server_usage",
 attribute "db_mysql/log_bin",
   :display_name => "MySQL Binlog Destination",
   :description => "Defines the filename and location of your MySQL stored binlog files.  This sets the log-bin variable in the MySQL config file. Ex: /mnt/mysql-binlogs/mysql-bin",
-  :recipes => ["db_mysql::default_server"],
+  :recipes => ["db_mysql::install_server"],
   :required => "optional",
   :default => "/mnt/ephemeral/mysql-binlogs/mysql-bin"
+
+attribute "db_mysql/tmpdir",
+  :display_name => "MySQL Temp Directory Destination",
+  :description => "Defines the location of your MySQL temp directory.  This sets the tmpdir variable in the MySQL config file. Ex: /tmp",
+  :recipes => ["db_mysql::install_server"],
+  :required => "optional",
+  :default => "/mnt/ephemeral/tmp"
 
