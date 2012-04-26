55e54826d751a18cbfc9281b69b427bd045df1e7
Erik Kargard <erik@rightscale.com>
2012-04-26 14:49:07 +0000

    [sys_log] New sys_log cookbook - empty

---
+++ cookbooks/sys_log/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures sys_log"
+
+version          "0.1"
