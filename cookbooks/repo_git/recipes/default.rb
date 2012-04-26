782f5618f0ee41be90779177e9562027dad4fc71
Douglas Thrift <douglas.thrift@rightscale.com>
2012-04-26 22:25:45 +0000

    Remove a merge artifact.

--- cookbooks/repo_git/recipes/default.rb
+++ cookbooks/repo_git/recipes/default.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #
 # Cookbook Name:: repo_git
 # Recipe:: default
@@ -24,29 +23,3 @@ unless node[:platform] == "mac_os_x" then
 end
 
 rs_utils_marker :end
-=======
-#
-# Cookbook Name:: repo_git
-#
-# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
-# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
-# if applicable, other agreements such as a RightScale Master Subscription Agreement.
-
-rs_utils_marker :begin
-
-unless node[:platform] == "mac_os_x" then
-  # Install git client
-  case node[:platform]
-  when "debian", "ubuntu"
-    package "git-core"
-  else 
-    package "git"
-  end
-
-  package "gitk"
-  package "git-svn"
-  package "git-email"
-end
-
-rs_utils_marker :end
->>>>>>> master
